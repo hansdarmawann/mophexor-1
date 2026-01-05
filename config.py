2a3bede862249c632a0e6833978c958355005e5b
Hans Darmawan <work.hansdarmawan001@gmail.com>
2026-01-05 09:21:42 +0700

    Add samples and exiftools directly.

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
-ROOT_DIR = Path(r"C:\Users\U1\Documents\Digikam")
-EXIFTOOL_PATH = Path(r"C:\Users\U1\Pictures\Sample")
+# Folder containing photos
+ROOT_DIR = Path("sample")
+
+# ExifTool binary
+EXIFTOOL_PATH = Path("exiftool-13.45_64/exiftool(-k).exe")
 
 MIN_MP4_SIZE = 100 * 1024
 IMAGE_EXTENSIONS = {".jpg", ".heic"}

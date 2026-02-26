1ebca3d5f28e3ca301d214a41368114728eb6c67
Hans Darmawan <work.hansdarmawan001@gmail.com>
2026-02-26 23:05:51 +0700

    Improve behavior, so not manually entering.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 ROOT_DIR = Path("sample")
 
 # ExifTool binary
-EXIFTOOL_PATH = Path("exiftool-13.45_64/exiftool(-k).exe")
+EXIFTOOL_PATH = Path("exiftool-13.45_64/exiftool.exe")
 
 MIN_MP4_SIZE = 100 * 1024
 IMAGE_EXTENSIONS = {".jpg", ".heic"}
